b96d774fd7e15cd81040492018224846d9c878da
Marek Kubica <marek@xivilization.net>
2018-07-26 13:14:46 +0200

    Add interface and mark things private

---
+++ lib/graphite_async.mli
@@ -0,0 +1,58 @@
+open Core_kernel
+open Async_kernel
+module Amqp = Amqp_client_async
+
+module Percentile : sig
+  type ts = int
+  type value = int
+  type t
+  val init : period:int -> percentiles:float list -> t
+  val average : int Array.t -> int
+  val get_stats : now:int -> t -> (string * value) list
+  val add_observation : t -> now:ts -> value -> unit
+end
+
+type t
+
+val init :
+  ?updates_per_minute:int ->
+  ?percentile_period:int ->
+  ?percentiles:float list ->
+  prefix:string -> Amqp.Connection.t -> t Deferred.t
+val incr : ?by:int -> key:string Hashtbl.key -> t -> unit
+val decr : t -> key:string Hashtbl.key -> int -> unit
+val incr_opt :
+  ?by:int ->
+  key:string Hashtbl.key -> t Option.t -> unit
+val decr_opt :
+  t Option.t -> key:string Hashtbl.key -> int -> unit
+val set : t -> key:string Hashtbl.key -> int -> unit
+val remove : t -> key:string Hashtbl.key -> unit
+val add_percentile_observation :
+  t -> key:string Hashtbl.key -> Percentile.value -> unit
+val add_percentile_observation_opt :
+  t option -> key:string Hashtbl.key -> Percentile.value -> unit
+
+module Result : sig
+  val time :
+    ?graphite:t ->
+    key:string ->
+    f:('a -> ('b, 'c) Deferred.Result.t) ->
+    'a -> ('b, 'c) Deferred.Result.t
+end
+
+module Option : sig
+  val time :
+    ?graphite:t ->
+    key:string ->
+    f:('a -> 'b option Deferred.t) ->
+    'a -> 'b option Deferred.t
+end
+
+module Deferred : sig
+  val time :
+    ?graphite:t ->
+    key:string ->
+    f:('a -> 'b Deferred.t) ->
+    'a -> 'b Deferred.t
+end
